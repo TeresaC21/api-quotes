ad83fdb9969d2a74c2b6231b7730e7e5d166e198
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-10-18 18:13:54 -0500

    Updates

---
+++ precache-manifest.1b8ddc783e2510884e6191d5d63f47ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e84aec119dcfdb6040082586d02cee",
+    "url": "/api-quotes/index.html"
+  },
+  {
+    "revision": "83b06ca294ffbe4de7cd",
+    "url": "/api-quotes/static/css/main.32642ee1.chunk.css"
+  },
+  {
+    "revision": "0d838d0853bf6b56cef4",
+    "url": "/api-quotes/static/js/2.5e074f6e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/api-quotes/static/js/2.5e074f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b06ca294ffbe4de7cd",
+    "url": "/api-quotes/static/js/main.3ed0601d.chunk.js"
+  },
+  {
+    "revision": "5250338bed3c1e99d263",
+    "url": "/api-quotes/static/js/runtime-main.21529f01.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/api-quotes/static/media/logo.427a4adc.svg"
+  }
+]);
